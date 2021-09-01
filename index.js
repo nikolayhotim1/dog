a8a6bc616fbc03ea89eef3ad31452ad771ef7265
nikolayhotim1 <hotim_nikolay@mail.ru>
2021-09-01 21:41:15 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,18 @@
+'usestrict';
+var dog = {
+    name: "Fido",
+    weight: 20.2,
+    age: 4,
+    breed: "mixed",
+    activity: "fetch balls"
+};
+
+var bark;
+if (dog.weight > 20) {
+    bark = "WOOF WOOF";
+} else {
+    bark = "woof woof";
+}
+
+var speak = dog.name + " says " + bark + " when he wants to " + dog.activity;
+console.log(speak); // Fido says WOOF WOOF when he wants to fetch balls
